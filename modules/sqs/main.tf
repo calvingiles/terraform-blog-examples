f06d8b6c756362dd62f00b1a437bf9643dd69f14
Calvin Giles <calvin.giles@gmail.com>
2019-09-21 08:57:32 +0100

    Format policy more nicely

--- modules/sqs/main.tf
+++ modules/sqs/main.tf
@@ -1,6 +1,11 @@
 resource aws_sqs_queue main-queue {
 	name = "${var.name}-queue"
-	redrive_policy = "{\"deadLetterTargetArn\":\"${aws_sqs_queue.dead-letter-queue.arn}\",\"maxReceiveCount\":4}"
+	redrive_policy = <<EOF
+	{
+		"deadLetterTargetArn": "${aws_sqs_queue.dead-letter-queue.arn}",
+		"maxReceiveCount":4
+	}
+EOF
 }
 resource aws_sqs_queue dead-letter-queue {
 	name = "${var.name}-dead-letter-queue"
